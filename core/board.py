4af0db52b00641bf417bbda11cdb9b5e2f929aee
karina BRANTE <karina.brante@gmail.com>
2017-05-12 11:25:57 +0200

    test_board ok

--- core/board.py
+++ core/board.py
@@ -7,8 +7,24 @@ class Board(object):
         for l in range(lig):
             self.grid.append([])
             for c in range(col):
-                self.grid[l].append(Cell())
+                self.grid[l].append(Cell(l, c))
+
+    def get_grid(self):
+        return self.grid
+
+    def get_cell(self, lig, col):
+        return self.grid[lig][col]
+
+    def get_width(self):
+        return len(self.grid[0])
+
+
+    def get_height(self):
+        return len(self.grid)
 
 class Cell(object):
-    def __init__(self):
-        pass
+    def __init__(self, lig, col):
+        self.items = []
+        self.characters = []
+        self.lig = lig
+        self.col = col
